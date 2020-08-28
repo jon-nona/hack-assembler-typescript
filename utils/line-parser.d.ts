b25cfcdcd83134ecd25dc0a10832ce9380e55d8b
jon-nona <jon@nonacreative.com>
2020-08-28 09:59:03 +0000

    Published by publish-to-git ed5b472b27e87e670d777e08b3ada5cfcbcc670b (HEAD -> refs/heads/develop, refs/remotes/origin/develop, refs/remotes/origin/HEAD) chore(deps-dev): bump @babel/plugin-proposal-object-rest-spread (#12)

---
+++ utils/line-parser.d.ts
@@ -0,0 +1,5 @@
+export declare const removeEmpty: (lines: string[]) => string[];
+export declare const removeWhiteSpace: (lines: string[]) => string[];
+export declare const toLines: (input: string) => string[];
+export declare const fromLines: (values: string[]) => string;
+export declare const cleanCommentsAndRemoveBlankLines: (input: string) => string;
