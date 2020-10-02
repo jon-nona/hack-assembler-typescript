12cf57ed04c7cae07bd813bd371c5d326ecd1573
jon-nona <jon@nonacreative.com>
2020-10-02 07:14:39 +0000

    Published by publish-to-git cf146dc96cd94b0f998ccd454a09d3ee0b535f6b (HEAD -> refs/heads/develop, refs/remotes/origin/develop, refs/remotes/origin/HEAD) chore(deps-dev): bump @typescript-eslint/parser from 4.2.0 to 4.3.0 (#30)

---
+++ utils/string.d.ts
@@ -0,0 +1,1 @@
+export declare const leftPad: any;
---
+++ utils/types.d.ts
@@ -0,0 +1,12 @@
+export interface FileArguments {
+    inputFile: string;
+    outputFile: string;
+}
+export interface CInstructionValue {
+    dest?: string;
+    comp?: string;
+    jump?: string;
+}
+export interface SymbolTable {
+    [key: string]: number;
+}
